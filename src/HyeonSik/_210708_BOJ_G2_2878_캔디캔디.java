b8a4639348d911a6a06e668b1dbcfe295f2e8a64
thsgustlr0318 <thsgustlr0318@gmail.com>
2021-07-08 20:44:07 +0900

    Update _210708_BOJ_G2_2878_캔디캔디.java

--- src/HyeonSik/_210708_BOJ_G2_2878_캔디캔디.java
+++ src/HyeonSik/_210708_BOJ_G2_2878_캔디캔디.java
@@ -1,51 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
+// 캔디 개수 내림차순 정렬 후, 가장 받고 싶은 개수가 큰 것부터 수 줄여가기
+// HashMap 사용해서 받고 싶은 개수가 같은 수는 한번에 처리 
 public class _210708_BOJ_G2_2878_캔디캔디 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int M = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
-		int[] temp = new int[N];
-		int[] arr = new int[N + 1];
-		HashMap<Integer, Integer> hashmap = new HashMap<>();
+		// map: <받고 싶은 캔디 값, 중복 되는 개수>
+		HashMap<Integer, Integer> map = new HashMap<>();
+		HashSet<Integer> set = new HashSet<>();
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
-			hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
-			temp[i] = num;
+			map.put(num, map.getOrDefault(num, 0) + 1);
+			set.add(num);
 		}
-		Arrays.sort(temp);
-		for (int i = 0; i < N; i++)
-			arr[i] = temp[N - i - 1];
-		arr[N] = 0;
-		for (int index = 0; index < N; index++) {
-			if (arr[index] == arr[index + 1])
-				continue;
-			int need = (arr[index] - arr[index + 1]) * hashmap.get(arr[index]);
-			if (M >= need) {
-				M -= need;
-				hashmap.put(arr[index + 1], hashmap.getOrDefault(arr[index + 1], 0) + hashmap.get(arr[index]));
-				hashmap.remove(arr[index]);
-			} else {
-				int mother = M / hashmap.get(arr[index]);
-				int child = M % hashmap.get(arr[index]);
-				int tmp = hashmap.get(arr[index]);
-				hashmap.remove(arr[index]);
-
-				hashmap.put(arr[index] - mother, hashmap.getOrDefault(arr[index] - mother, 0) + tmp - child);
-				hashmap.put(arr[index] - mother - 1, hashmap.getOrDefault(arr[index] - mother - 1, 0) + child);
+		// 캔디 개수 중복 없애기
+		ArrayList<Integer> arr = new ArrayList<>(set);
+		Collections.sort(arr, Collections.reverseOrder());
+		// 마지막에 0 넣어주기
+		arr.add(0);
+		for (int index = 0; index < arr.size(); index++) {
+			// count: 가장 큰 캔디 수 -> 그 다음 큰 캔디 수로 바꾸기 위해 필요한 크기
+			int count = (arr.get(index) - arr.get(index + 1)) * map.get(arr.get(index));
+			// 가장 큰 수를 그 다음 수만큼 줄일 수 있다면 (ex. { 10, 10, 8, 6 } -> { 8, 8, 8, 6 })
+			if (M >= count) {
+				M -= count;
+				map.put(arr.get(index + 1), map.getOrDefault(arr.get(index + 1), 0) + map.get(arr.get(index)));
+				map.remove(arr.get(index));
+			}
+			// 가장 큰 수를 그 다음 수만큼 줄일 수 없다면 (ex. { 10, 10, 8, 6 } -> { 9, 9, 8, 6 })
+			else {
+				// mother: 모두 균등하게 줄 수 있는 크기, child: 균등하게 주고 남은 크기
+				int mother = M / map.get(arr.get(index));
+				int child = M % map.get(arr.get(index));
+				int tmp = map.get(arr.get(index));
+				// 현재 캔디 크기 다 지우기
+				map.remove(arr.get(index));
+				// 재설정
+				map.put(arr.get(index) - mother, map.getOrDefault(arr.get(index) - mother, 0) + tmp - child);
+				map.put(arr.get(index) - mother - 1, map.getOrDefault(arr.get(index) - mother - 1, 0) + child);
 				break;
 			}
 		}
-
 		long result = 0;
-		for (int num : hashmap.keySet()) {
-			result += (long) hashmap.get(num) * num * num;
+		for (int num : map.keySet()) {
+			result += (long) map.get(num) * num * num;
 		}
 		System.out.println(result);
 	}
@@ -65,13 +73,16 @@ public class _210708_BOJ_G2_2878_캔디캔디 {
 //		int N = Integer.parseInt(st.nextToken());
 //		int[] arr = new int[N];
 //		long result = 0;
+//		// total: 부족한 사탕 개수
 //		long total = -M;
 //		for (int i = 0; i < N; i++) {
 //			arr[i] = Integer.parseInt(br.readLine());
 //			total += arr[i];
 //		}
+//		// 오름차순 정렬
 //		Arrays.sort(arr);
 //		for (int index = 0; index < N; index++) {
+//			// num: 부족한 사탕 개수 / (남은 사람 수), 현재 사탕 개수 중 작은 값
 //			long num = Math.min(total / (N - index), arr[index]);
 //			result += num * num;
 //			total -= num;
