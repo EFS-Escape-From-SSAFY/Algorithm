0622214f8b49ebeaca2ffc4e00f24ef728e6cf32
김성민 <kimsm4524@naver.com>
2021-07-14 00:06:51 +0900

    0713 dfs,bfs

--- src/SeongMin/0713/bj_16947.java
+++ src/SeongMin/0713/bj_16947.java
@@ -4,15 +4,36 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class bj_16947 {
+    static boolean[] cycle;
+    static boolean[] visited;
+    static int[] result;
+    static ArrayList<Integer>[] arrays;
+
+    static void dfs(int start, int cur, int count) {
+        visited[cur] = true;
+        for (int i : arrays[cur]) {
+            if (count >= 2 && i == start) {
+                cycle[start] = true;
+                return;
+            }
+            if (!visited[i]) {
+                dfs(start, i, count + 1);
+            }
+        }
+        return;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        ArrayList<Integer>[] arrays = new ArrayList[N + 1];
+        cycle = new boolean[N + 1];
+        visited = new boolean[N + 1];
+        arrays = new ArrayList[N + 1];
+        result = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
             arrays[i] = new ArrayList<Integer>();
         }
-        int[] result = new int[N + 1];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -20,49 +41,38 @@ public class bj_16947 {
             arrays[a].add(b);
             arrays[b].add(a);
         }
-
-        Queue<Integer> away = new LinkedList<>();
         for (int i = 1; i < N + 1; i++) {
-            if (arrays[i].size() == 1) {
-                away.add(i);
+            Arrays.fill(visited, false);
+            dfs(i, i, 0);
+
+        }
+        ArrayList<Integer> edges = new ArrayList<>();
+        for (int i = 1; i < N + 1; i++) {
+            if (cycle[i] && arrays[i].size() > 2) {
+                edges.add(i);
             }
         }
-        boolean visited[] = new boolean[N + 1];
-        while (!away.isEmpty()) {
-            int start = away.poll();
-            visited[start] = true;
-            while (true) {
-                System.out.println(start);
-                if (arrays[start].size() > 2) {
-                    int sum = 0;
-                    int temp = 0;
-                    for (int t : arrays[start]) {
-                        if (!visited[t] && arrays[t].size() > 2) {
-                            temp=t;
-                            sum++;
-                        }
-                    }
-                    System.out.println(sum);
-                    if(sum==1){
-                        visited[temp] = true;
-                        start=temp;
-                    }else{
-                        break;
-                    }
-                }else{
-                    System.out.println("here");
-                    for(int t:arrays[start]){
-                        if(!visited[t]){
-                            start = t;
-                            visited[t]=true;
-                        }
+        Arrays.fill(visited, false);
+        for (int edge : edges) {
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(edge);
+            queue.add(1);
+            while (!queue.isEmpty()) {
+                int temp = queue.poll();
+                int depth = queue.poll();
+                for (int t : arrays[temp]) {
+                    if (!cycle[t] && !visited[t]) {
+                        visited[t] = true;
+                        result[t] = depth;
+                        queue.add(t);
+                        queue.add(depth + 1);
                     }
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++)
-            sb.append(visited[i] + " ");
+        for (int i = 1; i < N + 1; i++)
+            sb.append(result[i] + " ");
         System.out.println(sb);
     }
 }
