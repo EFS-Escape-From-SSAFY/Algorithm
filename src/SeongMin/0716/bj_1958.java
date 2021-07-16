4ffc5f42819b3ba40aacd917844100daaef5bb50
김성민 <kimsm4524@naver.com>
2021-07-16 23:21:54 +0900

    백트래킹

--- src/SeongMin/0716/bj_1958.java
+++ src/SeongMin/0716/bj_1958.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class bj_1958 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class bj_1958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = new String[3];
+        for(int idx=0;idx<3;idx++)
+            strings[idx]=br.readLine();
+        int maxsize=0;
+        for(String s:strings){
+            maxsize = Math.max(maxsize,s.length());
+        }
+        int[][][] dp = new int[strings[0].length()+1][strings[1].length()+1][strings[2].length()+1];
+        int cur=0;
+        for(int i=0;i<strings[0].length();i++){
+            for(int j=0;j<strings[1].length();j++){
+                for(int k=0;k<strings[2].length();k++){
+                    if(strings[0].charAt(i)==strings[1].charAt(j)&&strings[1].charAt(j)==strings[2].charAt(k)){
+                        dp[i+1][j+1][k+1]=dp[i][j][k]+1;
+                    }else{
+                        int temp = Math.max(dp[i+1][j+1][k],dp[i][j+1][k+1]);
+                        dp[i+1][j+1][k+1] = Math.max(temp,dp[i+1][j][k+1]);
+                    }
+
+                }
+            }
+        }
+        System.out.println(dp[strings[0].length()][strings[1].length()][strings[2].length()]);
+    }
 }
